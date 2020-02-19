d31acd7d772d4921f726d904807a540f082046f1
G. David Butler <gdavidbutler@users.noreply.github.com>
2020-02-19 22:29:30 +0000

    one more control character

--- json.c
+++ json.c
@@ -457,6 +457,7 @@ jsonEncodeString(
                                    case 0x0b:                       case 0x0e: case 0x0f:
   case 0x10: case 0x11: case 0x12: case 0x13: case 0x14: case 0x15: case 0x16: case 0x17:
   case 0x18: case 0x19: case 0x1a: case 0x1b: case 0x1c: case 0x1d: case 0x1e: case 0x1f:
+  case                                                                              0x7f:
     return (-1);
     break;
   case '\b': /* 0x08 */

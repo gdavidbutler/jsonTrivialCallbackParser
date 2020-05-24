3fc4e44965f136d24d0130ec0f4d0672cdb713df
G. David Butler <gdavidbutler@users.noreply.github.com>
2020-05-24 23:46:05 +0000

    adjust input by UTF multi-character count

--- json.c
+++ json.c
@@ -372,7 +372,8 @@ sur:
       } else
         in++;
       len++;
-    } else if ((*in & 0xe0) == 0xc0 /* 2 bytes */
+    } else if (ilen > 0
+            && (*in & 0xe0) == 0xc0 /* 2 bytes */
             && (*(in + 1) & 0xc0) == 0x80) {
       if (olen > 1) {
         *out++ = *in++;
@@ -381,7 +382,9 @@ sur:
       } else
         in += 2;
       len += 2;
-    } else if ((*in & 0xf0) == 0xe0 /* 3 bytes */
+      ilen--;
+    } else if (ilen > 1
+            && (*in & 0xf0) == 0xe0 /* 3 bytes */
             && (*(in + 1) & 0xc0) == 0x80
             && (*(in + 2) & 0xc0) == 0x80) {
       if (olen > 2) {
@@ -392,7 +395,9 @@ sur:
       } else
         in += 3;
       len += 3;
-    } else if ((*in & 0xf8) == 0xf0 /* 4 bytes */
+      ilen -= 2;
+    } else if (ilen > 2
+            && (*in & 0xf8) == 0xf0 /* 4 bytes */
             && (*(in + 1) & 0xc0) == 0x80
             && (*(in + 2) & 0xc0) == 0x80
             && (*(in + 3) & 0xc0) == 0x80) {
@@ -405,7 +410,9 @@ sur:
       } else
         in += 4;
       len += 4;
-    } else if ((*in & 0xfc) == 0xf8 /* 5 bytes */
+      ilen -= 3;
+    } else if (ilen > 3
+            && (*in & 0xfc) == 0xf8 /* 5 bytes */
             && (*(in + 1) & 0xc0) == 0x80
             && (*(in + 2) & 0xc0) == 0x80
             && (*(in + 3) & 0xc0) == 0x80
@@ -420,7 +427,9 @@ sur:
       } else
         in += 5;
       len += 5;
-    } else if ((*in & 0xfe) == 0xfc /* 6 bytes */
+      ilen -= 4;
+    } else if (ilen > 4
+            && (*in & 0xfe) == 0xfc /* 6 bytes */
             && (*(in + 1) & 0xc0) == 0x80
             && (*(in + 2) & 0xc0) == 0x80
             && (*(in + 3) & 0xc0) == 0x80
@@ -437,6 +446,7 @@ sur:
       } else
         in += 6;
       len += 6;
+      ilen -= 5;
     } else
       goto err;
     break;
@@ -461,7 +471,7 @@ jsonEncodeString(
                                    case 0x0b:                       case 0x0e: case 0x0f:
   case 0x10: case 0x11: case 0x12: case 0x13: case 0x14: case 0x15: case 0x16: case 0x17:
   case 0x18: case 0x19: case 0x1a: case 0x1b: case 0x1c: case 0x1d: case 0x1e: case 0x1f:
-  case                                                                              0x7f:
+                                                                               case 0x7f:
     return (-1);
     break;
   case '\b': /* 0x08 */
@@ -536,7 +546,8 @@ jsonEncodeString(
       } else
         in++;
       len++;
-    } else if ((*in & 0xe0) == 0xc0 /* 2 bytes */
+    } else if (ilen > 0
+            && (*in & 0xe0) == 0xc0 /* 2 bytes */
             && (*(in + 1) & 0xc0) == 0x80) {
       if (olen > 1) {
         *out++ = *in++;
@@ -545,7 +556,9 @@ jsonEncodeString(
       } else
         in += 2;
       len += 2;
-    } else if ((*in & 0xf0) == 0xe0 /* 3 bytes */
+      ilen--;
+    } else if (ilen > 1
+            && (*in & 0xf0) == 0xe0 /* 3 bytes */
             && (*(in + 1) & 0xc0) == 0x80
             && (*(in + 2) & 0xc0) == 0x80) {
       if (olen > 2) {
@@ -556,7 +569,9 @@ jsonEncodeString(
       } else
         in += 3;
       len += 3;
-    } else if ((*in & 0xf8) == 0xf0 /* 4 bytes */
+      ilen -= 2;
+    } else if (ilen > 2
+            && (*in & 0xf8) == 0xf0 /* 4 bytes */
             && (*(in + 1) & 0xc0) == 0x80
             && (*(in + 2) & 0xc0) == 0x80
             && (*(in + 3) & 0xc0) == 0x80) {
@@ -569,7 +584,9 @@ jsonEncodeString(
       } else
         in += 4;
       len += 4;
-    } else if ((*in & 0xfc) == 0xf8 /* 5 bytes */
+      ilen -= 3;
+    } else if (ilen > 3
+            && (*in & 0xfc) == 0xf8 /* 5 bytes */
             && (*(in + 1) & 0xc0) == 0x80
             && (*(in + 2) & 0xc0) == 0x80
             && (*(in + 3) & 0xc0) == 0x80
@@ -584,7 +601,9 @@ jsonEncodeString(
       } else
         in += 5;
       len += 5;
-    } else if ((*in & 0xfe) == 0xfc /* 6 bytes */
+      ilen -= 4;
+    } else if (ilen > 4
+            && (*in & 0xfe) == 0xfc /* 6 bytes */
             && (*(in + 1) & 0xc0) == 0x80
             && (*(in + 2) & 0xc0) == 0x80
             && (*(in + 3) & 0xc0) == 0x80
@@ -601,6 +620,7 @@ jsonEncodeString(
       } else
         in += 6;
       len += 6;
+      ilen -= 5;
     } else
       return (-1);
     break;
